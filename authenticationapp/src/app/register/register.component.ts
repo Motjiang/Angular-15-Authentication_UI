2c10bc19f0486223e7b104281d310937687a3213
Motjiang <motjiangbrilliant@gmail.com>
2024-10-31 14:52:10 +0200

    Created the CRU operations

--- authenticationapp/src/app/register/register.component.ts
+++ authenticationapp/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private builder: FormBuilder, private toastr: ToastrService){
+
+  }
+
+  registerForm = this.builder.group({
+    id:this.builder.control('',Validators.compose([Validators.required, Validators.minLength(5)])),
+    name:this.builder.control('',Validators.required),
+    password:this.builder.control('',Validators.compose([Validators.required, Validators.pattern('?=. *[a-z]) (?=. *[A-Z]) (?=.*[0-9]) (?=.*[$@$!%*?&]) [A-Za-z\d$@$ !%*?&]. {8,}')])),
+    email:this.builder.control('',Validators.compose([Validators.required, Validators.email])),
+    gender:this.builder.control('male',),
+    role:this.builder.control(''),
+    isactive:this.builder.control(false),
+  });
+
+
+  proceedregistration(){
+    if(this.registerForm.valid){
+      this.toastr.success('Registration Successfull');
+    }else{
+      this.toastr.warning('Please enter valid data');
+    }
+  }
 
 }
--- authenticationapp/src/app/service/auth.service.ts
+++ authenticationapp/src/app/service/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,25 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+   }
+
+   apiurl = 'http://localhost:3000/user';
+
+   GetAllUsers(){
+     return this.http.get(this.apiurl);
+   }
+
+   GetUserById(code:any){
+     return this.http.get(this.apiurl+'/'+code);
+   }
+
+   RegisterUser(data:any){
+     return this.http.post(this.apiurl,data);
+   }
+
+    UpdateUser(code:any,data:any){
+      return this.http.put(this.apiurl+'/'+code,data);
+    }
 }
