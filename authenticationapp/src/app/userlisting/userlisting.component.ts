d82cbbb549b5e692bf394f2e4cbda1ccb9d2913a
Motjiang <motjiangbrilliant@gmail.com>
2024-10-31 18:47:53 +0200

    added Pagination

--- authenticationapp/src/app/userlisting/userlisting.component.ts
+++ authenticationapp/src/app/userlisting/userlisting.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-userlisting',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./userlisting.component.css']
 })
 export class UserlistingComponent {
+
+  constructor( private service: AuthService) { 
+    this.LoadUserList();
+
+  }
+
+  userlist:any;
+  dataSource:any;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  LoadUserList(){
+    this.service.GetAllUsers().subscribe(res=>{
+      this.userlist = res;
+      this.dataSource = new MatTableDataSource(this.userlist);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  
+  displayedColumns: string[] = ['role', 'name', 'email', 'status', 'action'];
+  editUser(code:any){}
 
 }
